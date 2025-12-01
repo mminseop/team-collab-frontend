82a0d607b1459fa0b7fbe50c29d2d3ca78d8d4b8
mminseop <alstjq903@naver.com>
2025-12-02 00:01:29 +0900

    Feat:  sass 패키지 재설치

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src")],
-  },
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;

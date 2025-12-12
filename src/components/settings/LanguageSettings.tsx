ca4af0230f70a196bddcbcb7536effc7231455cc
mminseop <alstjq903@naver.com>
2025-12-12 19:13:46 +0900

    Feat: 언어설정 컴포넌트 추가

---
+++ src/components/settings/LanguageSettings.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import st from "./SettingsDialog.module.scss";
+
+export function LanguageSettings() {
+  return (
+    <div className={st.contentSection}>
+      <h2>언어</h2>
+      <div className={st.formGroup}>
+        <label>표시 언어</label>
+        <select className={st.select}>
+          <option>한국어</option>
+          <option>English</option>
+          <option>日本語</option>
+        </select>
+      </div>
+      <div className={st.settingItem}>
+        <div className={st.settingInfo}>
+          <h3>자동 번역</h3>
+          <p>다른 언어로 작성된 메시지 자동 번역</p>
+        </div>
+        <label className={st.switch}>
+          <input type="checkbox" />
+          <span className={st.slider}></span>
+        </label>
+      </div>
+    </div>
+  );
+}
